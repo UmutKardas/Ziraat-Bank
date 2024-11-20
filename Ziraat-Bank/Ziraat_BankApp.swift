b3403f9019f73887495a081bf61d6f67f4f249b4
Hüseyin Umut Kardaş <kardasumt@gmail.com>
2024-11-20 22:48:35 +0300

    Initial Commit

---
+++ Ziraat-Bank/Ziraat_BankApp.swift
@@ -0,0 +1,17 @@
+//
+//  Ziraat_BankApp.swift
+//  Ziraat-Bank
+//
+//  Created by Hüseyin Umut Kardaş on 20.11.2024.
+//
+
+import SwiftUI
+
+@main
+struct Ziraat_BankApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
